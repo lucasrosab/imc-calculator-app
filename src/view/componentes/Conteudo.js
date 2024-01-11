33ef060c44166b2f0f49bd605198361d6f5b8d03
Lucas Rosa <lucasrosa.comercial@gmail.com>
2024-01-10 23:30:14 -0300

    Versao final do projeto

--- src/view/componentes/Conteudo.js
+++ src/view/componentes/Conteudo.js
@@ -1,12 +1,43 @@
-import React from "react";
-import { Keyboard, StyleSheet, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
+import React, { useEffect } from "react";
+import { Alert, Keyboard, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
 import dados from "../../model/mock/conteudoMock"
 import Texto from "./Texto";
+import { useForm } from "react-hook-form"
 
 export default function Conteudo() {
 
-    const [number, onChangeNumber] = React.useState('');
+    const { register, setValue, handleSubmit } = useForm();
 
+    const onSubmit = (data) => {
+        var alturaMetros = data.altura / 100
+        var imc = parseInt(data.peso / (alturaMetros * alturaMetros))
+        var response = "";
+
+        if (imc < 17) {
+            response = "muito abaixo do peso";
+        } else if (imc >= 17 && imc < 19) {
+            response = "abaixo do peso";
+        } else if (imc >= 19 && imc < 25) {
+            response = "peso normal";
+        } else if (imc >= 25 && imc < 30) {
+            response = "acima do peso";
+        } else if (imc >= 30 && imc < 35) {
+            response = "obesidade grau I";
+        } else if (imc >= 35 && imc < 40) {
+            response = "obesidade grau II";
+        } else if (imc >= 40) {
+            response = "obesidade grau III";
+        }
+
+
+        Alert.alert("Resultado", `Seu IMC deu ${ imc }, este resultado é considerado ${ response }`)
+    }    
+
+    useEffect(() => {
+        register('peso')
+        register('altura')
+      }, [register])
+    
     return <>
         <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
             <View>
@@ -17,6 +48,7 @@ export default function Conteudo() {
                     placeholder={ dados.campoPeso }
                     maxLength={ 3 }
                     keyboardType="number-pad"
+                    onChangeText={e => setValue("peso", e)}
                 />
             </View>
         </TouchableWithoutFeedback> 
@@ -30,11 +62,12 @@ export default function Conteudo() {
                     placeholder={ dados.campoAltura }
                     maxLength={ 3 }
                     keyboardType="number-pad"
+                    onChangeText={e => setValue("altura", e)}
                 />
             </View>
         </TouchableWithoutFeedback> 
 
-        <TouchableOpacity style={ estilos.btn }>
+        <TouchableOpacity style={ estilos.btn } onPress={handleSubmit(onSubmit)}>
             <Texto customDesign={ estilos.btnTitle }>{ dados.btnCalcular }</Texto>
         </TouchableOpacity>
     </>
